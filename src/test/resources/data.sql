3f704d3fcfd5e544c1bdf1c8c05f0ce601f117c4
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-08 19:34:52 +0200

    logging queries + scripts

---
+++ src/test/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO EMPLOYEE
+VALUES (1, 'Warsaw', 'Plac Zbawiciela', 'Tumilowicz');
+
+INSERT INTO EMPLOYEE
+VALUES (2, 'Krakow', 'Plac Szczepanski', 'Mrozek');
+
+INSERT INTO EMPLOYEE
+VALUES (3, 'Krakow', null, 'Lem');
+
+INSERT INTO EMPLOYEE
+VALUES (4, 'Key West', '907 Whitehead St', 'Hemingway');
+
+INSERT INTO ISSUE
+VALUES (1, 'stay sharp!', 1);
+
+INSERT INTO ISSUE
+VALUES (2, 'improve coding skills', 1);
+
+INSERT INTO ISSUE
+VALUES (3, 'prepare newest novel for editing', 2);
+
+COMMIT;
